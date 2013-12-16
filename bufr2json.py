f5e4bc918c2b552a79bebf218dafc3589920b1c2
Tuure Laurinolli <tuure.laurinolli@fmi.fi>
2013-12-16 17:22:30 +0200

    Moved into package

--- bufr2json.py
+++ bufr2json.py
@@ -1,16 +1,17 @@
-import bufrdec
+import bufrpy
 import json
 import sys
 import time
+import codecs
 
-b_table = bufrdec.read_safnwc_template(open(sys.argv[1], 'rb'))
+b_table = bufrpy.read_safnwc_template(codecs.open(sys.argv[1], 'rb', 'utf-8'))
 t_0 = time.time()
 
-msg = bufrdec.bufrdec_file(open(sys.argv[2], 'rb'), b_table)
+msg = bufrpy.bufrdec_file(open(sys.argv[2], 'rb'), b_table)
 t_1 = time.time()
-out = json.dumps(bufrdec.to_json(msg))
+out = json.dumps(bufrpy.to_json(msg))
 t_2 = time.time()
-dec = bufrdec.from_json(json.loads(out))
+dec = bufrpy.from_json(json.loads(out))
 t_3 = time.time()
 
 print out
---
+++ bufrpy/__init__.py
@@ -0,0 +1,1 @@
+from bufrpy.bufrdec import *
