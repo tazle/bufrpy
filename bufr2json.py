f37735170d7381ebd1b998d11e9efd0d1143dfa3
Tuure Laurinolli <tuure.laurinolli@fmi.fi>
2013-12-19 14:59:12 +0200

    Try to use libbufr-format parser as well for parsing tables/templates

--- bufr2json.py
+++ bufr2json.py
@@ -2,12 +2,18 @@ from __future__ import print_function
 
 import bufrpy
 from bufrpy.template import safnwc
+from bufrpy.table import libbufr
 import json
 import sys
 import time
 import codecs
 
-b_table = safnwc.read_template(codecs.open(sys.argv[1], 'rb', 'utf-8'))
+try:
+    # First try reading as safnwc template
+    b_table = safnwc.read_template(codecs.open(sys.argv[1], 'rb', 'utf-8'))
+except Exception as e:
+    # Try reading as libbufr table
+    b_table = libbufr.read_b_table(codecs.open(sys.argv[1], 'rb', 'utf-8'))
 t_0 = time.time()
 
 msg = bufrpy.bufrdec_file(open(sys.argv[2], 'rb'), b_table)
