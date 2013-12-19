007921a05d84bdc907a4c5db7fcc850e7d63672f
Tuure Laurinolli <tuure.laurinolli@fmi.fi>
2013-12-19 13:28:03 +0200

    Make BufrMessage available through the main module

--- bufrpy/__init__.py
+++ bufrpy/__init__.py
@@ -5,7 +5,7 @@ Based on FM 94 BUFR edition 3 as found in WMO306_vl2_BUFR3_Spec_en.pdf
 
 """
 
-from bufrpy.bufrdec import bufrdec, bufrdec_file, Section0, Section1, Section2, Section3, Section4, Section5
+from bufrpy.bufrdec import bufrdec, bufrdec_file, Section0, Section1, Section2, Section3, Section4, Section5, BufrMessage
 
 from bufrpy.json import from_json, to_json
 
@@ -14,4 +14,4 @@ __author__ = 'Tuure Laurinolli / FMI'
 __version__ = "0.1"
 __copyright__ = 'Copyright 2013 Finnish Meteorological Institute'
 
-__all__ = ["from_json", "to_json", "bufrdec", "bufrdec_file", "Section0", "Section1", "Section2", "Section3", "Section4", "Section5"]
+__all__ = ["from_json", "to_json", "bufrdec", "bufrdec_file", "Section0", "Section1", "Section2", "Section3", "Section4", "Section5", "BufrMessage"]
