a528336c9af11b86d2dfd7b0174b67eb80d896cc
Tuure Laurinolli <tuure.laurinolli@fmi.fi>
2013-12-30 14:38:33 +0200

    HRW to JSON converter, demonstrated use of bufrdec_all

---
+++ hrw2json.py
@@ -0,0 +1,16 @@
+from __future__ import print_function
+
+import bufrpy
+from bufrpy.template import safnwc
+from bufrpy.table import libbufr
+from bufrpy.util import ByteStream
+import json
+import sys
+import codecs
+
+# Try reading as libbufr table
+b_table = libbufr.read_b_table(codecs.open(sys.argv[1], 'rb', 'utf-8'))
+
+stream = ByteStream(open(sys.argv[2], 'rb'))
+messages, errors = bufrpy.bufrdec_all(stream, b_table)
+print(json.dumps(messages))
