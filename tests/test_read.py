80a1fe13c4942df9f0f2f4b3f1007518a4417726
Tuure Laurinolli <tuure.laurinolli@fmi.fi>
2014-01-16 18:38:38 +0200

    Fix context manager usage for compatibility with 2.6

--- tests/test_read.py
+++ tests/test_read.py
@@ -16,9 +16,10 @@ class TestReadBufr(unittest.TestCase):
 
     def _read(self, b_table_file, d_table_file, bufr_file):
         b_file = codecs.open(b_table_file, 'rb', 'utf-8')
-        d_file = codecs.open(d_table_file, 'rb', 'utf-8')
-        with b_file, d_file:
-            table = libbufr.read_tables(b_file, d_file)
-            f = open(bufr_file, 'rb')
-            with f:
-                return bufrpy.bufrdec_file(f, table)
+        with b_file:
+            d_file = codecs.open(d_table_file, 'rb', 'utf-8')
+            with d_file:
+                table = libbufr.read_tables(b_file, d_file)
+                f = open(bufr_file, 'rb')
+                with f:
+                    return bufrpy.bufrdec_file(f, table)
