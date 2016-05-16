35679ca59b76fe7efe2a7c84cfb68180610dec90
Tuure Laurinolli <tuure@laurinolli.net>
2016-05-16 23:25:34 +0300

    Release 0.2.2 with pnuu's fixes for setup process and updated e-mail address

--- bufrpy/__init__.py
+++ bufrpy/__init__.py
@@ -11,7 +11,7 @@ from bufrpy.json import from_json, to_json
 
 __title__ = 'bufrpy'
 __author__ = 'Tuure Laurinolli / FMI'
-__version__ = "0.2.1"
-__copyright__ = 'Copyright 2013 Finnish Meteorological Institute'
+__version__ = "0.2.2"
+__copyright__ = 'Copyright 2013-2016 Finnish Meteorological Institute, Tuure Laurinolli'
 
 __all__ = ["from_json", "to_json", "decode", "decode_file", "decode_all", "Section0", "Section1v3", "Section1v4", "Section2", "Section3", "Section4", "Section5", "Message"]
